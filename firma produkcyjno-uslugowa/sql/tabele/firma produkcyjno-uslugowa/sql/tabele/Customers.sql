f6f807155d8700d0675e5faf22f519f5e507f167
mikimiki102 <mwalek102@gmail.com>
2026-01-04 16:46:40 +0100

    Delete firma produkcyjno-uslugowa/sql/tabele/firma produkcyjno-uslugowa/sql/tabele directory

--- firma produkcyjno-uslugowa/sql/tabele/firma produkcyjno-uslugowa/sql/tabele/Customers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dbo.Customers(
-    CustomerID int IDENTITY(1, 1) NOT NULL CONSTRAINT PK_Customers PRIMARY KEY,
-    Name Nvarchar(200) NOT NULL,
-    Address Nvarchar(200) NOT NULL,
-    PhoneNumber Nvarchar(20) NOT NULL
-)
