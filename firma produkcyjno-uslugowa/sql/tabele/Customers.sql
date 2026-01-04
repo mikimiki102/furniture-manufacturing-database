1d385123b198f8e0d14819d14392859c0412c901
mikimiki102 <mwalek102@gmail.com>
2026-01-04 17:29:01 +0100

    Update Customers.sql
    Dodanie tabeli Customers

--- firma produkcyjno-uslugowa/sql/tabele/Customers.sql
+++ firma produkcyjno-uslugowa/sql/tabele/Customers.sql
@@ -1,6 +1,13 @@
-CREATE TABLE dbo.Customers(
-    CustomerID int IDENTITY(1, 1) NOT NULL CONSTRAINT PK_Customers PRIMARY KEY,
-    Name Nvarchar(200) NOT NULL,
-    Address Nvarchar(200) NOT NULL,
-    PhoneNumber Nvarchar(20) NOT NULL
-)
+CREATE TABLE dbo.Customers
+(
+    CustomerID  INT IDENTITY(1, 1) NOT NULL CONSTRAINT PK_Customers PRIMARY KEY,
+    Name        NVARCHAR(200) NOT NULL,
+    Address     NVARCHAR(200) NOT NULL,
+    PhoneNumber NVARCHAR(20)  NOT NULL
+        CONSTRAINT CK_Customers_Correct_Phone
+        CHECK (
+            PhoneNumber LIKE '+[0-9]%'
+            AND PhoneNumber NOT LIKE '%[^0-9+]%'
+            AND LEN(PhoneNumber) BETWEEN 10 AND 16
+        )
+);
