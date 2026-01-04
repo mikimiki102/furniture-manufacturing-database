f4b8b15b7556bd7430c834b282160aca9b728aa7
Mikolaj Walek <mwalek102@gmail.com>
2026-01-04 16:24:59 +0100

    Dodanie tabeli Products

---
+++ firma produkcyjno-uslugowa/sql/tabele/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE dbo.Products
+(
+    ProductID      INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Products PRIMARY KEY,
+    ProductName    NVARCHAR(200) NOT NULL,
+    Price          DECIMAL(10, 2) NOT NULL
+        CONSTRAINT CK_Products_Price_NonNegative CHECK (Price >= 0),
+    ProductionTime INT NOT NULL
+        CONSTRAINT CK_Products_ProductionTime_Positive CHECK (ProductionTime > 0)
+);
