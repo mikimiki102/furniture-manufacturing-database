9b32e4abe509616ae328351ac9a63ca1985b5e19
mikimiki102 <mwalek102@gmail.com>
2026-01-16 18:22:50 +0100

    Dodanie OrderCount_ByCustomerID.sql

---
+++ firma produkcyjno-uslugowa/sql/funkcje/OrderCount_ByCustomerID.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER FUNCTION dbo.fn_OrderCount_ByCustomerID (@CustomerID INT)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @cnt INT;
+  SELECT @cnt = COUNT(*)
+  FROM dbo.Orders
+  WHERE CustomerID = @CustomerID;
+
+  RETURN ISNULL(@cnt, 0);
+END
+GO
