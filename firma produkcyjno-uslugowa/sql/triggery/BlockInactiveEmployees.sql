75fc185340f25c4623d7e63d65a5eff77948abcd
Kacper Kulka <kuleczkomen@gmail.com>
2026-01-26 19:07:22 +0100

    triggery zrobione

--- firma produkcyjno-uslugowa/sql/triggery/BlockInactiveEmployees.sql
+++ firma produkcyjno-uslugowa/sql/triggery/BlockInactiveEmployees.sql
@@ -6,7 +6,19 @@ on dbo.Orders
         set nocount on;
 
         declare @InactiveEmployeeID int;
-        declare @ErrorMessage NVARCHAR(100);
+        declare @ErrorMsg NVARCHAR(100);
 
-        select top 1 @InactiveEmployeeID = sfrom ;
+        select top 1 @InactiveEmployeeID = e.EmployeeID
+        from inserted i
+        inner join Employees e
+            on i.EmployeeID = e.EmployeeID
+        where e.isActive = 0;
+
+        if @InactiveEmployeeID is not null
+        begin
+            set @ErrorMsg = concat(
+                            'Nie można przypisać nieaktywnego pracownika (ID: ', cast(@InactiveEmployeeID as NVARCHAR(10)), ') do zamówienia'
+                            );
+            throw 50001, @ErrorMsg, 1;
+        end
     end
