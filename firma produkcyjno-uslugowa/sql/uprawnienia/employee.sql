35d63df32058c5138eb8d248ad277e135136e7e5
mikimiki102 <mwalek102@gmail.com>
2026-01-28 20:31:13 +0100

    Update employee.sql

--- firma produkcyjno-uslugowa/sql/uprawnienia/employee.sql
+++ firma produkcyjno-uslugowa/sql/uprawnienia/employee.sql
@@ -4,7 +4,6 @@ grant select, insert, update on Orders to employee;
 grant select, insert, update on OrderDetails to employee;
 grant select on Products to employee;
 
--- Row-Level Security (tylko swój rekord)
 grant select, update on Employees to employee;
 
 create security policy dbo.EmployeePolicy
