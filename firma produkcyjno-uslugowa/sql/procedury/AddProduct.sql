7e1380c1e5cc3baf97c3161b1875a7dda4e8b9b5
mikimiki102 <mwalek102@gmail.com>
2026-01-28 16:07:06 +0100

    Update AddProduct.sql

--- firma produkcyjno-uslugowa/sql/procedury/AddProduct.sql
+++ firma produkcyjno-uslugowa/sql/procedury/AddProduct.sql
@@ -1,13 +1,14 @@
 CREATE   PROCEDURE dbo.AddProduct
   @ProductName     NVARCHAR(200),
   @Price           DECIMAL(10,2),
-  @ProductionTime  INT
+  @ProductionTime  INT,
+  @PartID          INT
 AS
 BEGIN
   SET NOCOUNT ON;
 
   INSERT INTO dbo.Products (ProductName, Price, ProductionTime)
-  VALUES (@ProductName, @Price, @ProductionTime);
+  VALUES (@ProductName, @Price, @ProductionTime, @PartID);
 
 END
 go
