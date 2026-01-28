7aea991c653f00d7fc1561707d21a61920137ddb
xxdagi <pasik.dagmara1706@gmail.com>
2026-01-28 12:24:20 +0100

    przeniesienie pliku

--- firma produkcyjno-uslugowa/sql/uprawnienia/client.sql
+++ firma produkcyjno-uslugowa/sql/uprawnienia/client.sql
@@ -1,8 +1,6 @@
 CREATE ROLE client;
 
 GRANT SELECT ON Products to client;
-GRANT SELECT ON v_CustomerOrderHistory to client;
-GRANT SELECT ON v_OrderSchedule to client;
 
 GRANT EXECUTE ON AddCustomer to client;
 GRANT EXECUTE ON ChangeCustomerContact  to client;
