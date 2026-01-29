c57f09f60397d30a6a50bad0bfe33368ddd7738f
xxdagi <pasik.dagmara1706@gmail.com>
2026-01-29 21:58:05 +0100

    poprawa uprawnień dla logistyki

--- firma produkcyjno-uslugowa/sql/uprawnienia/logistics.sql
+++ firma produkcyjno-uslugowa/sql/uprawnienia/logistics.sql
@@ -1,20 +1,10 @@
 CREATE ROLE logistics;
 
-GRANT SELECT ON dbo.Orders TO logistics;
+GRANT SELECT, UPDATE ON dbo.Orders TO logistics;
 GRANT SELECT ON dbo.Customers TO logistics;
-GRANT UPDATE ON dbo.Shippers TO logistics;
+GRANT SELECT, INSERT, UPDATE ON dbo.Shippers TO logistics;
+GRANT SELECT, UPDATE ON dbo.OrderExecution TO logistics;
 
-GRANT SELECT ON dbo.v_OrderSchedule TO logistics;
-GRANT SELECT ON dbo.v_CustomerOrderHistory TO logistics;
-GRANT SELECT ON dbo.v_OrderBasic TO logistics;
-
-GRANT INSERT, UPDATE, SELECT ON dbo.OrderExecution TO logistics;
-
-GRANT EXECUTE ON AddShipper TO logistics;
-GRANT EXECUTE  ON ChangeShipperData to logistics;
-GRANT EXECUTE ON ChangeShipperContact to logistics;
-GRANT EXECUTE ON ChangeIsActiveShipper to logistics;
-GRANT EXECUTE ON ChangeOrderData TO logistics;
 
 
 
