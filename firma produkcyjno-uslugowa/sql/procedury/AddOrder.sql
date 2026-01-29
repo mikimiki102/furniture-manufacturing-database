d2ff7c954a85d2a9c62e85dc30ea2b56c1511550
mikimiki102 <mwalek102@gmail.com>
2026-01-29 17:09:09 +0100

    Update AddOrder.sql

--- firma produkcyjno-uslugowa/sql/procedury/AddOrder.sql
+++ firma produkcyjno-uslugowa/sql/procedury/AddOrder.sql
@@ -1,22 +1,21 @@
-CREATE PROCEDURE dbo.AddOrder
+CREATE OR ALTER PROCEDURE dbo.AddOrder
     @CustomerID INT,
     @EmployeeID INT,
     @ShipperID INT,
     @OrderDate DATETIME,
-    @DeliveryDate DATETIME = NULL,
+    @DeliveryDate DATETIME,
     @Address NVARCHAR(200),
     @Country NVARCHAR(100),
     @City NVARCHAR(100)
 AS
-    BEGIN
-        SET NOCOUNT ON;
+BEGIN
+    SET NOCOUNT ON;
 
-        INSERT INTO dbo.Orders
+    INSERT INTO dbo.Orders
         (CustomerID, EmployeeID, ShippersID, OrderDate, DeliveryDate, Address, Country, City)
-        VALUES
-        (@CustomerID,@EmployeeID, @ShipperID, @OrderDate,
-         @DeliveryDate, @Address, @Country, @City);
+    VALUES
+        (@CustomerID, @EmployeeID, @ShipperID, @OrderDate, @DeliveryDate, @Address, @Country, @City);
 
-        SELECT CAST(SCOPE_IDENTITY() AS INT) AS OrderID;
-    END;
-go
+    SELECT CAST(SCOPE_IDENTITY() AS INT) AS OrderID;
+END
+GO
