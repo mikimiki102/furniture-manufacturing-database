e53fad9514085ef72add2d78e8f93c6e6ef39f0f
mikimiki102 <mwalek102@gmail.com>
2026-01-29 17:06:44 +0100

    Update AddPayment.sql

--- firma produkcyjno-uslugowa/sql/procedury/AddPayment.sql
+++ firma produkcyjno-uslugowa/sql/procedury/AddPayment.sql
@@ -1,18 +1,18 @@
-CREATE PROCEDURE dbo.AddPayment
+CREATE OR ALTER PROCEDURE dbo.AddPayment
     @OrderID INT,
-    @Amount INT,
+    @Amount DECIMAL(10,2),
     @STATUS INT,
     @Method NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
 
-    AS
-    BEGIN
-        SET NOCOUNT ON;
+    DECLARE @out TABLE (PaymentID INT);
 
-        INSERT INTO dbo.Payments(OrderID, PaymentDate, Amount, STATUS, Method)
-        OUTPUT inserted.PaymentID
-        VALUES (@OrderID, GETDATE(), @Amount, @STATUS, @Method)
+    INSERT INTO dbo.Payments (OrderID, PaymentDate, Amount, STATUS, Method)
+    OUTPUT inserted.PaymentID INTO @out(PaymentID)
+    VALUES (@OrderID, GETDATE(), @Amount, @STATUS, @Method);
 
-
-    end
-go
-
+    SELECT PaymentID FROM @out;
+END
+GO
